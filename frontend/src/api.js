8cd0b1592923ea1f372fa5debd31ab463e2c76eb
Rizky Hafizh Jatmiko <Rizky0409j@gmail.com>
2024-04-17 20:35:51 +0700

    moving package in package.json and change port

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 // eslint-disable-next-line no-undef
-// ngrok
-// const apiUrl = 'https://492f-103-28-114-170.ngrok-free.app/'
-const apiUrl = 'http://localhost:3456/'
+const apiUrl = 'http://localhost:3000/'
 
 export const getProductList = async () => {
     const product = await axios.get(apiUrl)
